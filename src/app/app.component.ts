bfa5af9864504dc736afec7fbf055d095affb958
LouisKzy <louiskrejezy@gmail.com>
2024-07-17 10:16:02 +0200

    course angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent],
+
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="assets/logo.png"
+          alt="logo"
+          aria-hidden="true"
+          width="80"
+          height="80"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
 })
 export class AppComponent {
-  title = 'my-new-app-angular';
+  title = 'Wazaaa';
 }
